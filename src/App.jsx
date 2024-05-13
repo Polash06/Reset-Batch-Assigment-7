55177e1c0ce028a3feffd81e142b21227bb7c046
polasho6 <hossainpolash009@gmail.com>
2024-05-13 09:41:35 +0600

    Tailwin And DaisuyUi set up and navber section complet

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./componets/Navbar/Navbar";
+
+function App() {
+
+  return <>
+  
+  <div className="mx-10 mt-8">
+    <Navbar></Navbar>
+  </div>
+  
+  </>;
+}
+
+export default App;
---
+++ src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+
+const Navbar = () => {
+    return (
+        <div>
+     <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <li className="text-xl font-medium"><a>Home</a></li>
+      <li className="text-xl font-medium"><a>Recipes</a></li>
+      <li className="text-xl font-medium"><a>About</a></li>
+      <li className="text-xl font-medium"><a>Search</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost lg:text-4xl font-bold">Recipe Calories</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+   
+    <li className="text-xl font-medium"><a>Home</a></li>
+      <li className="text-xl font-medium"><a>Recipes</a></li>
+      <li className="text-xl font-medium"><a>About</a></li>
+      <li className="text-xl font-medium"><a>Search</a></li>
+    </ul>
+  </div>
+  
+  <div className="navbar-end gap-8">
+  <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+    <div className="w-16 ">
+          <img className="rounded-full" alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
